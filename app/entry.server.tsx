2065fbdde71f12af21a8b62a6fbcf792366116c9
Afees Kolawole <dcodesmith@gmail.com>
2024-12-06 21:19:33 +0000

    switch package manager to pnpm

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,5 +1,4 @@
 import { PassThrough } from "stream";
-
 import type { EntryContext } from "@remix-run/node";
 import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
