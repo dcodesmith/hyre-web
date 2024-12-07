0966f08798333920238807f5ada0cc9b91b8e30a
Afees Kolawole <dcodesmith@gmail.com>
2024-12-07 00:14:13 +0000

    swap homepage route

--- app/routes/items.tsx
+++ app/routes/items.tsx
@@ -3,18 +3,20 @@
 // import invariant from "tiny-invariant";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData, useSearchParams } from "@remix-run/react";
 import {
-  ColumnFilter,
+  Link,
+  redirect,
+  useLoaderData,
+  useSearchParams,
+} from "@remix-run/react";
+import {
   ColumnFiltersState,
-  ColumnSort,
   getCoreRowModel,
   getFacetedRowModel,
   getFacetedUniqueValues,
   getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
-  InitialTableState,
   PaginationState,
   SortingState,
   useReactTable,
@@ -26,21 +28,39 @@ import Carousel from "~/components/Carousel";
 import { columns } from "~/components/Table/Columns";
 import { Pagination } from "~/components/Table/Pagination";
 import { Toolbar } from "~/components/Table/Toolbar";
+import { URLFacetedFilter } from "~/components/Table/URLFacetedFilter";
+import { requireSessionUser, requireUser } from "~/modules/auth/auth.server";
 import { prisma } from "~/modules/db/db.server";
 
 import type { SerializedCar } from "~/types";
+import { userHasRole } from "~/utils/misc";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  // const user = await requireUser(request);
+
+  // if (userHasRole(user, "fleetOwner")) {
+  //   return redirect("/fleet-owner");
+  // }
+
+  // await requireUser(request, { redirectTo: "/fleet-owner" });
+  // await requireSessionUser(request, { redirectTo: "/fleet-owner" });
+
   const url = new URL(request.url);
   const from = url.searchParams.get("from");
   const to = url.searchParams.get("to");
-
+  const makes = url.searchParams.getAll("make");
+  const models = url.searchParams.getAll("model");
+  console.log(makes);
   const cars = await prisma.car.findMany({
     where: {
-      // make: {
-      //   mode: "insensitive",
-      //   in: makes.length > 0 ? makes : undefined,
-      // },
+      make: {
+        mode: "insensitive",
+        in: makes.length > 0 ? makes.toString().split(",") : undefined,
+      },
+      model: {
+        mode: "insensitive",
+        in: models.length > 0 ? models.toString().split(",") : undefined,
+      },
       OR: [
         // Available cars with no bookings
         {
@@ -54,25 +74,8 @@ export async function loader({ request }: LoaderFunctionArgs) {
               status: { in: ["PENDING", "CONFIRMED", "ACTIVE"] },
               OR: [
                 {
-                  // startDate: { lte: to ? new Date(to) : undefined },
-                  // endDate: { gte: from ? new Date(from) : undefined },
-                  // startDate: { lt: to ? new Date(to) : undefined },
-                  // endDate: { gt: from ? new Date(from) : undefined },
-                  // startDate: {
-                  //   lt: to ? new Date(`${to}T00:00:00Z`) : undefined,
-                  // },
-                  // endDate: {
-                  //   gt: from ? new Date(`${from}T00:00:00Z`) : undefined,
-                  // },
-                  // startDate: { lte: to ? new Date(to) : undefined },
-                  // endDate: { gte: from ? new Date(from) : undefined },
-
-                  startDate: {
-                    lte: to ? new Date(`${to}T23:59:59Z`) : undefined,
-                  },
-                  endDate: {
-                    gte: from ? new Date(`${from}T00:00:00Z`) : undefined,
-                  },
+                  startDate: { lte: to ? new Date(to) : undefined },
+                  endDate: { gte: from ? new Date(from) : undefined },
                 },
               ],
             },
@@ -85,197 +88,119 @@ export async function loader({ request }: LoaderFunctionArgs) {
     },
   });
 
-  return json({ cars });
+  const allMakes = await prisma.car
+    .findMany({
+      where: {
+        model: {
+          in: models.length > 0 ? models.toString().split(",") : undefined,
+        },
+      },
+      distinct: ["make"],
+      select: { make: true },
+    })
+    .then((makes) => makes.map((m) => m.make));
+
+  const allModels = await prisma.car
+    .findMany({
+      where: {
+        make: {
+          in: makes.length > 0 ? makes.toString().split(",") : undefined,
+        },
+      },
+      distinct: ["model"],
+      select: { model: true },
+    })
+    .then((models) => models.map((m) => m.model));
+
+  return json({
+    cars,
+    makes: allMakes,
+    models: allModels,
+  });
 }
 
-const PAGE_SIZE = 12;
-
-// Utility to convert table state to search params
-const convertTableStateToSearchParams = (tableState: InitialTableState) => {
-  const params = new URLSearchParams();
-  // Persist column filters
-  if (tableState.columnFilters && tableState.columnFilters.length > 0) {
-    tableState.columnFilters.forEach((filter) => {
-      params.set(`filter.${filter.id}`, filter.value as string);
-    });
-  }
-
-  // Persist sorting
-  if (tableState.sorting && tableState.sorting.length > 0) {
-    tableState.sorting.forEach((sort) => {
-      params.set(`sort.${sort.id}`, sort.desc ? "desc" : "asc");
-    });
-  }
-
-  return params;
-};
-
-// Utility to parse search params back to table state
-const parseSearchParamsToTableState = (urlSearchParams: URLSearchParams) => {
-  const columnFilters: ColumnFilter[] = [];
-  const sorting: ColumnSort[] = [];
-
-  for (const [key, value] of urlSearchParams.entries()) {
-    if (key.startsWith("filter.")) {
-      const columnId = key.replace("filter.", "");
-      columnFilters.push({ id: columnId, value: value.split(",") });
-    }
-
-    if (key.startsWith("sort.")) {
-      const columnId = key.replace("sort.", "");
-      sorting.push({ id: columnId, desc: value === "desc" });
-    }
-  }
-
-  return { columnFilters, sorting };
-};
+// const PAGE_SIZE = 12;
 
 export default function IndexPage() {
   const { cars } = useLoaderData<typeof loader>();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
-  const from = searchParams.get("from");
-  const to = searchParams.get("to");
+  const [searchParams] = useSearchParams();
+  // const [sorting, setSorting] = useState<SortingState>([]);
+  // const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  // // const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  // const [pagination, setPagination] = useState<PaginationState>({
+  //   pageIndex: 0,
+  //   pageSize: PAGE_SIZE,
+  // });
+  // const table = useReactTable<SerializedCar>({
+  //   data: cars,
+  //   columns,
+  //   state: {
+  //     sorting,
+  //     columnVisibility,
+  //     // columnFilters,
+  //     pagination,
+  //   },
+  //   getCoreRowModel: getCoreRowModel(),
+  //   getPaginationRowModel: getPaginationRowModel(),
+  //   getSortedRowModel: getSortedRowModel(),
+  //   getFacetedUniqueValues: getFacetedUniqueValues(),
+  //   getFacetedRowModel: getFacetedRowModel(),
+  //   getFilteredRowModel: getFilteredRowModel(),
 
-  // Initialize table state from URL search params
-  const initialTableState = parseSearchParamsToTableState(searchParams);
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
-    initialTableState.columnFilters
-  );
-  const [sorting, setSorting] = useState<SortingState>(
-    initialTableState.sorting
-  );
+  //   onSortingChange: setSorting,
+  //   // onColumnFiltersChange: setColumnFilters,
+  //   onColumnVisibilityChange: setColumnVisibility,
+  //   onPaginationChange: setPagination,
+  // });
 
-  const [pagination, setPagination] = useState<PaginationState>({
-    pageIndex: 0,
-    pageSize: PAGE_SIZE,
-  });
-
-  const table = useReactTable<SerializedCar>({
-    data: cars,
-    columns,
-    state: {
-      sorting,
-      columnVisibility,
-      columnFilters,
-      pagination,
-    },
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFacetedUniqueValues: getFacetedUniqueValues(),
-    getFacetedRowModel: getFacetedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-
-    onSortingChange: (updater) => {
-      const newSorting =
-        typeof updater === "function" ? updater(sorting) : updater;
-
-      setSorting(newSorting);
-
-      // Update URL search params when sorting changes
-      const params = convertTableStateToSearchParams({
-        columnFilters,
-        sorting: newSorting,
-      });
-
-      setSearchParams(params);
-    },
-    onColumnFiltersChange: (updater) => {
-      const newFilters =
-        typeof updater === "function" ? updater(columnFilters) : updater;
-
-      setColumnFilters(newFilters);
-
-      const params = convertTableStateToSearchParams({
-        columnFilters: newFilters,
-        sorting,
-      });
-
-      //setSearchParams(params);
-
-      setSearchParams((prev) => {
-        const existingParams = new URLSearchParams(prev);
-        const newParams = new URLSearchParams(params);
-
-        // Merge new params with existing ones
-        for (const [key, value] of newParams) {
-          existingParams.set(key, value);
-        }
-
-        return existingParams;
-      });
-    },
-    onColumnVisibilityChange: setColumnVisibility,
-    onPaginationChange: setPagination,
-  });
-
-  const calculateTotalDays = () => {
-    if (!from || !to) {
-      return 1;
-    }
-
-    // If both dates are the same day, return 1
-    if (
-      new Date(from).toLocaleDateString() === new Date(to).toLocaleDateString()
-    ) {
-      return 1;
-    }
-
-    // Add 1 to include both the start and end dates
-    const days =
-      Math.ceil(
-        (new Date(to).getTime() - new Date(from).getTime()) / (1000 * 3600 * 24)
-      ) + 1;
-
-    return days;
-  };
+  // const makes = [...new Set(table.options.data.map(({ make }) => make))];
+  // const models = [...new Set(table.options.data.map(({ model }) => model))];
 
   return (
     <div className="max-w-8xl mx-auto space-y-4">
-      <Toolbar table={table} />
+      {/* <Toolbar table={table} /> */}
 
-      {table.getRowModel().rows.length ? (
+      {/* <URLFacetedFilter
+        title="Make"
+        paramKey="make"
+        options={makes.map((make) => ({
+          label: make,
+          value: make,
+        }))}
+      />
+
+      <URLFacetedFilter
+        title="Model"
+        paramKey="model"
+        options={models.map((model) => ({
+          label: model,
+          value: model,
+        }))}
+      /> */}
+
+      {cars.length ? (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {table.getRowModel().rows.map((row) => (
+          {cars.map((car) => (
             <Link
-              key={row.original.id}
-              to={`/cars/${row.original.id}?${searchParams.toString()}`}
+              key={car.id}
+              to={`/cars/${car.id}?${searchParams.toString()}`}
               className="block"
             >
               <div className="rounded overflow-hidden hover:shadow-lg transition-shadow">
-                <Carousel
-                  images={
-                    row.original.images.length ? row.original.images : undefined
-                  }
-                />
+                <Carousel images={car.images.length ? car.images : undefined} />
 
                 <div className="p-4">
                   <h2 className="font-semibold mb-2">
-                    {row.original.make} {row.original.model} (
-                    {row.original.year})
+                    {car.make} {car.model} ({car.year})
                   </h2>
 
-                  <p>
-                    {!from || !to ? (
-                      <>
-                        {new Intl.NumberFormat("en-NG", {
-                          style: "currency",
-                          currency: "NGN",
-                        }).format(row.original.price)}
-                      </>
-                    ) : (
-                      <>
-                        Booking total:{" "}
-                        <span className="font-bold underline">
-                          {new Intl.NumberFormat("en-NG", {
-                            style: "currency",
-                            currency: "NGN",
-                          }).format(row.original.price * calculateTotalDays())}
-                        </span>
-                      </>
-                    )}
+                  <p className="font-bold">
+                    {new Intl.NumberFormat("en-NG", {
+                      style: "currency",
+                      currency: "NGN",
+                    }).format(car.price)}
                   </p>
+                  {/* {car.owner.username} */}
                 </div>
               </div>
             </Link>
@@ -285,12 +210,12 @@ export default function IndexPage() {
         <div>No cars matching your search criteria.</div>
       )}
 
-      {table.getFilteredRowModel().rows.length > PAGE_SIZE && (
+      {/* {table.getFilteredRowModel().rows.length > PAGE_SIZE && (
         <Pagination
           range={[PAGE_SIZE, PAGE_SIZE * 2, PAGE_SIZE * 3]}
           table={table}
         />
-      )}
+      )} */}
     </div>
   );
 }
