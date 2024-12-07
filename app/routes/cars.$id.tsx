265686c86cef7e298a2f975352b283653849cdc9
Afees Kolawole <dcodesmith@gmail.com>
2024-12-07 10:41:07 +0000

    naviagte back to search results on home page

--- app/routes/cars.$id.tsx
+++ app/routes/cars.$id.tsx
@@ -147,7 +147,7 @@ export default function CarDetails() {
   return (
     <div className="max-w-6xl mx-auto p-4 space-y-4">
       <Link
-        to={`/items/?${searchParams.toString()}`}
+        to={`/?${searchParams.toString()}`}
         className="text-blue-500 hover:underline mb-1 inline-block"
       >
         &larr; Back to search results
