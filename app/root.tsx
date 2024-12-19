e92f7f2f6d1132e09b32c4b37a048718fbb5e71d
Afees Kolawole <dcodesmith@gmail.com>
2024-12-19 18:21:16 +0000

    add title tag

--- app/root.tsx
+++ app/root.tsx
@@ -54,6 +54,9 @@ export default function App() {
   return (
     <html lang="en" className="h-full">
       <head>
+        <title>
+          {ENV.APP_NAME} - Effortless, Reliable, Safe, and Exceptional Service.
+        </title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
