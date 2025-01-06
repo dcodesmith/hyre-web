e9e5f0048b34181c884a380a5d238e205c1cd722
Afees Kolawole <dcodesmith@gmail.com>
2025-01-06 07:58:02 +0000

    fix transport

--- app/lib/logger.server.ts
+++ app/lib/logger.server.ts
@@ -1,15 +1,20 @@
 import pino from "pino";
 
-const logger = pino({
-  level: process.env.NODE_ENV === "development" ? "debug" : "info",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      ignore: "pid,hostname",
-      translateTime: "yyyy-mm-dd HH:MM:ss",
-    },
-  },
-});
+const transport =
+  process.env.NODE_ENV !== "production"
+    ? {
+        level: process.env.NODE_ENV === "development" ? "debug" : "info",
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            ignore: "pid,hostname",
+            translateTime: "yyyy-mm-dd HH:MM:ss",
+          },
+        },
+      }
+    : undefined;
+
+const logger = pino(transport);
 
 export default logger;
