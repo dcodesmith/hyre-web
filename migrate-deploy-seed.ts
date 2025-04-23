fa3f6cec01d365566929bd6b04d4842fd2f29c65
Afees Kolawole <dcodesmith@gmail.com>
2025-04-23 19:02:04 +0100

    seed everytime

--- migrate-deploy-seed.ts
+++ migrate-deploy-seed.ts
@@ -9,17 +9,17 @@ exec("npx prisma migrate deploy", (error, stdout, stderr) => {
   // Log all migration output
   console.log("Migration Output:", stdout);
 
-  if (stdout.includes("No pending migrations to apply")) {
-    console.log("No migrations were needed");
-  } else {
-    // Run your seed script here
-    exec("npx prisma db seed", (seedError, seedStdout, seedStderr) => {
-      if (seedError) {
-        console.error(`Error running seed script: ${seedStderr}`);
-        process.exit(1);
-      }
-      // Log seed output
-      console.log("Seed Output:", seedStdout);
-    });
-  }
+  // if (stdout.includes("No pending migrations to apply")) {
+  //   console.log("No migrations were needed");
+  // } else {
+  // Run your seed script here
+  exec("npx prisma db seed", (seedError, seedStdout, seedStderr) => {
+    if (seedError) {
+      console.error(`Error running seed script: ${seedStderr}`);
+      process.exit(1);
+    }
+    // Log seed output
+    console.log("Seed Output:", seedStdout);
+  });
+  // }
 });
